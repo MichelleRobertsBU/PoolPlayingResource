59a21f99dbd15d98a274921a39a289c459a68bd1
Michelle Roberts <80293351+MichelleRobertsBU@users.noreply.github.com>
2022-07-07 16:41:47 -0400

    Add project files.

---
+++ PoolPlayingResource/Models/Venue.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PoolPlayingResource.Models
+{
+    public class Venue
+
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
+
